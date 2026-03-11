055246a3dc1b68632ad496fa7f5caf1aa1f9293e
Local Dev <localdev@example.com>
2026-03-11 10:33:22 +0300

    Fix Pages workflow and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/weather-app-3d/',
   plugins: [react()],
-  resolve: {
-    alias: {
-    },
-  },
   server: {
     proxy: {
       '/api': {
